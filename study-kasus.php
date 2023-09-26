668ace17c64d2d5c291188b656ec396171c19dc6
korius kussa wenda <koriuswenda@gmail.com>
2023-09-26 17:49:03 +0700

    study kasus

---
+++ study-kasus.php
@@ -0,0 +1,42 @@
+<?php
+// PERHITUNGAN
+
+function tambah($a, $b)
+{
+    $c = $a + $b;
+    return $c;
+}
+echo " 10 + 7 = " . tambah(10,7). "<br>";
+
+
+// PERKALIAN
+function kali($a, $b)
+{
+$c = $a * $b;
+return $c;
+}
+echo "5 * 5 =" . kali(5,5). "<br>";
+
+
+//PENGURANGAN
+function kurang($b, $c)
+{
+    $d = $b - $c;
+    return $d;
+}
+echo "7-5 =" . kurang(7,5 ). "<br>";
+
+
+
+function bagi($d, $e , )
+{
+    $f = $d / $e;
+    $g=$d.'/'. $d . "=". $e . "<br>";
+    return $g;
+}
+echo bagi(20,2);
+
+
+
+
+?>
