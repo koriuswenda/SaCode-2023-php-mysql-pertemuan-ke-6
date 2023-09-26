22e6d62bbe3d1dfe8d04469c118713e2e62f73fb
korius kussa wenda <koriuswenda@gmail.com>
2023-09-26 17:14:02 +0700

    function Return

---
+++ function-type-data.php
@@ -0,0 +1,11 @@
+<?php
+
+function angka(int $a, int $b)
+{
+    $c = $a + $b;
+    echo $c;
+}
+
+angka (5,5);
+
+?>
---
+++ function-type-return.php
@@ -0,0 +1,8 @@
+<?php
+
+function hitung(int $a, int $b)
+{
+   return $c = $a + $b ;
+}
+echo hitung(12,7)
+  ?>
